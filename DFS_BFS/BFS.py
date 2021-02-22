f738bf45b4ff3569eddb22e3315a7991a1200716
Lee-Geon-Yeong <yellowisher@naver.com>
2021-02-22 14:56:10 +0900

    commit

--- DFS_BFS/BFS.py
+++ DFS_BFS/BFS.py
@@ -10,12 +10,14 @@ def bfs(graph, start, visited):
     while queue:
         # 큐에서 하나의 원소를 뽑아 출력
         v=queue.popleft()
-        print(v, end=' ')
+        result.append(v)
+        # print(v, end=' ')
         # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
                 visited[i]=True
+    
 
 graph=[
     [],
@@ -31,6 +33,7 @@ graph=[
 
 # 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
 visited=[False]*9
-
+result=[]
 # 정의된 DFS 함수 호출
 bfs(graph, 1, visited)
+print(result)
--- DFS_BFS/practice.py
+++ DFS_BFS/practice.py
@@ -1,2 +1,41 @@
-min=[0]*(4+1)
-print(min)
+from collections import deque
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+q = deque()
+for i in range(M):
+	for j in range(N):
+		if graph[i][j] == 1:
+			q.append([i, j])
+
+while q:
+	x, y = q.popleft()
+
+	for i in range(4):
+		xx = dx[i] + x
+		yy = dy[i] + y	
+
+		if M > xx >= 0 and N > yy >= 0:
+			if graph[xx][yy] == 0:
+				graph[xx][yy] = 1
+				graph[xx][yy] = graph[x][y] + 1
+				q.append([xx, yy])
+
+result = -2
+check_graph = False
+
+for i in graph:
+	for j in i:
+		if j == 0:
+			check_graph = True
+		result = max(result, j)
+if check_graph:
+	print(-1)
+elif result == -1:
+	print(0)
+else:
+	print(result - 1)
