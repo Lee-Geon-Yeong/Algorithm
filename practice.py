e23a4155e7dc641f72245876cfd1ea43a4900881
Lee-Geon-Yeong <yellowisher@naver.com>
2021-03-18 13:04:28 +0900

    commit

--- practice.py
+++ practice.py
@@ -1,9 +1,2 @@
-m=input()
-
-for i in range(1, len(m)+1):
-    new=[]
-    k=m[0:i]
-    for j in range(i, len(m), i+1):
-        print(i, j)
-        if k==m[i:j]
-# print(m[0:1])
+dx=[0,1,2,3]
+print(dx[-1])
