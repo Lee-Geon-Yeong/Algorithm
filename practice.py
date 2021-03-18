858e7cd386743bcaf893559ae630bdf5031a2570
Lee-Geon-Yeong <yellowisher@naver.com>
2021-03-18 11:01:14 +0900

    commit

--- practice.py
+++ practice.py
@@ -1,3 +1,9 @@
-import math
-n=10000
-print(log2(n)*n)
+m=input()
+
+for i in range(1, len(m)+1):
+    new=[]
+    k=m[0:i]
+    for j in range(i, len(m), i+1):
+        print(i, j)
+        if k==m[i:j]
+# print(m[0:1])
