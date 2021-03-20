4630d1ce1b44d5b0a3ec04a95e3f6cc17f96b5d6
Lee-Geon-Yeong <yellowisher@naver.com>
2021-03-20 12:21:19 +0900

    commit

--- Implementation/기둥과보설치.py
+++ Implementation/기둥과보설치.py
@@ -1,4 +1,4 @@
-def possible(anwer):
+def possible(answer):
     for x, y, stuff in answer:
         if stuff==0:
             if y==0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y, -1, 0] in answer:
